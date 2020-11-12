28eb2bab61d0b0938e10ca358998aca82409af80
Benjamin <benbuzz68@gmail.com>
2020-11-12 14:43:31 -0500

    fixed crap

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
+import pyrebase
 import io
 import os
-import myTest
+import predict
+
+config = {
+    "apiKey": "AIzaSyCNREzzNW83QsUIgG6QzX-ozpOINWL955c",
+    "authDomain": "security-whale.firebaseapp.com",
+    "databaseURL": "https://security-whale.firebaseio.com",
+    "projectId": "security-whale",
+    "storageBucket": "security-whale.appspot.com",
+    "messagingSenderId": "774091153639",
+    "appId": "1:774091153639:web:a54a1397c23f22242e0242",
+    "measurementId": "G-6VD698LMTY"
+}
 
 firebase = pyrebase.initialize_app(config)
-
 
 app = Flask(__name__)
 CORS(app)
@@ -17,9 +28,9 @@ def index():
         data_as_string = str(data, "utf-8")
         if not data:
             return jsonify({"error":"no file"})
-        return myTest.makePrediction(data_as_string), 201
+        return predict.makePrediction(data_as_string), 201
     else:
-        return jsonify({"about":"Hello World!"})
+        return jsonify({"about":"Nothing to see here..."})
 
 # def index():
 #     return render_template('index.html')

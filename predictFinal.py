f46a318bc4f4979f4c8d5a4f041be084ff4d1113
Kyle Sizemore <kylerusssizemore@gmail.com>
2020-11-24 18:24:31 -0500

    Added spec, predict changes, new model

--- predictFinal.py
+++ predictFinal.py
@@ -12,12 +12,18 @@ from tensorflow.keras.layers import Activation, Dense, Embedding, GlobalAverageP
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 from tensorflow.keras import layers
 
-def makePrediction(messages_as_string):
+def switch (modelSelection):
+    switcher = {
+        '1/50': './assets/models/model_vul1_nonvul50.h5'
+    }
+    return switcher.get(modelSelection,'invalidModel')
+
+def makePrediction(messages_as_string, modelSelection):
     print ("Running prediction function...")
     messages = list(messages_as_string.split('s3cur!tywh@l3'))
 
-    vocab_size = 12612
-    sequence_length = 1000
+    vocab_size = 15613 # amount of words that appear in commit messages
+    sequence_length = 1000 # arbitrary vector length
 
     embedding_layer = tf.keras.layers.Embedding(vocab_size, sequence_length)
 
@@ -28,11 +34,13 @@ def makePrediction(messages_as_string):
     text_ds = tf.data.Dataset.from_tensor_slices(messages).batch(32)
     vectorizer.adapt(text_ds)
 
-    path = './assets/models/model.h5'
+    #switch case for select
+    path = switch(modelSelection)
+    if path == 'invalidModel': return path
+    #path = './assets/models/' + modelSelection
     print ("trying to load model at: " + path)
     model = load_model(path)
     print ("I loaded a model")
-
     string_input = keras.Input(shape=(1,), dtype="string")
     x = vectorizer(string_input)
     preds = model(x)
@@ -47,13 +55,18 @@ def makePrediction(messages_as_string):
         probabilities = end_to_end_model.predict(
             [[message]]
             )
+        print(message)
         vulnProbabilitySum = vulnProbabilitySum + probabilities[0][1]
+        print('vuln:',probabilities[0][1])
         nonVulnProbabilitySum = nonVulnProbabilitySum + probabilities[0][0]
+        print('nonvuln:',probabilities[0][0])
     vulnLikelyHoodStr = '0' if count == 0 else str(vulnProbabilitySum/count)
     nonVulnLikelyHoodStr = '0' if count == 0 else str(nonVulnProbabilitySum/count)
     ## confidence should equal approximately 1
-    confindence = '0' if count == 0 str(vulnProbabilitySum/count + nonVulnProbabilitySum/count)
-    return_string = vulnStr + ',' + nonVulnStr + ',' + confindence
+    confindence = '0' if count == 0 else str(vulnProbabilitySum/count + nonVulnProbabilitySum/count)
+    return_string = vulnLikelyHoodStr + ',' + nonVulnLikelyHoodStr + ',' + confindence
     print("Response body: \n" + return_string)
 
     return return_string
+modelSelection = '1/50'
+makePrediction('thisisTest huhuhuhuhuhuhuh brazil number one',modelSelection)
